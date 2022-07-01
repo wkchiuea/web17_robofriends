6cf8b2902081acbce7af8015f79627a72ae02393
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-07-01 13:35:55 +0800

    complete 4 react hooks lifecycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import {robots} from "./robots";
+// import {robots} from "./robots";
 import "./App.css";
 
 
@@ -9,9 +9,15 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => {this.setState({robots: users})});
     }
 
     onSearchChange = (event) => {
@@ -23,13 +29,17 @@ class App extends Component {
             return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
         })
 
-        return (
-            <div className="tc">
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>LOADING</h1>;
+        } else {
+            return (
+                <div className="tc">
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
